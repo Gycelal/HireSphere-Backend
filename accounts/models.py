0bfaaaa6caf43d029e4c6f9708873bb63350deb6
Gycelal <krgycelal@gmail.com>
2026-02-14 12:18:24 +0530

    feat(auth): implement custom User model and configure authentication

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('candidate', 'Candidate'),
+        ('recruiter', 'Recruiter'))
+    
+    username = None
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    is_verified = models.BooleanField(default=False)
+    is_approved = models.BooleanField(default=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
--- hire_sphere/settings.py
+++ hire_sphere/settings.py
@@ -29,6 +29,8 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+AUTH_USER_MODEL = "accounts.User"
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
