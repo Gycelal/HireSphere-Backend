e0410a4abb3766fa2daeea4fea4b1fd31dcdfb21
Gycelal <krgycelal@gmail.com>
2026-04-02 17:00:52 +0530

    feat(recruiter): profile update

--- recruiter_panel/views.py
+++ recruiter_panel/views.py
@@ -13,3 +13,12 @@ class RecruiterProfileView(RetrieveUpdateAPIView):
     def get_object(self):
         profile, created = RecruiterProfile.objects.get_or_create(user=self.request.user)
         return profile
+    
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+    
