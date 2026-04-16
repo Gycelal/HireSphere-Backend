2b5994077a38af0a0d2d86ccdff2121cf48882ee
Gycelal <krgycelal@gmail.com>
2026-04-16 12:10:16 +0530

    feat: add permission class IsApprovedAdmin

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -8,4 +8,8 @@ class IsCandidate(BasePermission):
     def has_permission(self, request, view):
         return request.user and request.user.is_authenticated and request.user.role == 'candidate'
     
+class IsApprovedRecruiter(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated and request.user.role == 'recruiter' and request.user.approval_status == 'approved'
+    
     
