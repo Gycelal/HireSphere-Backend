8aec6ad195f80df832222f3e42f106e43abc9cfb
Gycelal <krgycelal@gmail.com>
2026-04-21 21:28:17 +0530

    feat(recruiter_profile): Remove profile_completion method from model and Add get_completion_percentage method in serializer level

--- recruiter_panel/serializers.py
+++ recruiter_panel/serializers.py
@@ -18,10 +18,28 @@ class RecruiterProfileSerializer(serializers.ModelSerializer):
     
 class UserProfileSerializer(serializers.ModelSerializer):
     profile = RecruiterProfileSerializer(source="recruiterprofile", required=False)
+    completion_percentage = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ["first_name", "last_name", "email", "profile"]
+        fields = ["first_name", "last_name", "email", "profile", "completion_percentage"]
+
+    def get_completion_percentage(self, user):
+        profile = getattr(user, "recruiterprofile", None)
+        fields = [
+            user.first_name,
+            user.last_name,
+            user.email,
+        ]
+        profile_fields = RecruiterProfileSerializer.Meta.fields
+        if profile:
+            fields.extend([getattr(profile, field) for field in profile_fields])
+        else:
+            fields.extend([None] * len(profile_fields)) 
+        
+        completed = sum(1 for field in fields if field)
+        total = len(fields)
+        return int((completed/total) * 100) if total > 0 else 0
 
     def update(self, user, validated_data):
         profile_data = validated_data.pop("recruiterprofile", None)
