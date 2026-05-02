063d6539ce0dacd6414d6a21f3e9774b0b283700
Gycelal <krgycelal@gmail.com>
2026-05-02 20:36:27 +0530

    feat(recruiter_panel): Add RECRUITER_TYPES choices on recruiter_type field

--- recruiter_panel/models.py
+++ recruiter_panel/models.py
@@ -5,10 +5,17 @@ from django.conf import settings
 
 
 class RecruiterProfile(models.Model):
+
+    RECRUITER_TYPES = [
+        ('company', 'Company'),
+        ('individual', 'Individual'),
+        ('agency', 'Agency'),
+    ]
+    
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="recruiterprofile")
     display_name = models.CharField(max_length=255, blank=True)
     profile_picture = models.URLField(blank=True)
-    recruiter_type = models.CharField(max_length=255, blank=True)
+    recruiter_type = models.CharField(max_length=255, blank=True, choices=RECRUITER_TYPES)
     company_or_brand_name = models.CharField(max_length=255, blank=True)
     website_url = models.URLField(blank=True)
     # subscription_plan = models.ForeignKey(on_delete=models.SET_NULL, null=True, blank=True, related_name="recruiterprofile")
