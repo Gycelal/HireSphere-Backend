8ec4a8a9dc2881754446397c5f5b91738ce45b77
Gycelal <krgycelal@gmail.com>
2026-05-22 19:53:48 +0530

    WIP

--- recruiter_panel/views.py
+++ recruiter_panel/views.py
@@ -1,13 +1,11 @@
 from rest_framework.generics import RetrieveUpdateAPIView
 from accounts.permissions import IsRecruiter
-from .serializers import RecruiterProfileSerializer, UserProfileSerializer
-from  .models import RecruiterProfile
-from accounts.models import User
-from rest_framework.response import Response
-from rest_framework import status
-
+from .serializers import  UserProfileSerializer
+import cloudinary.uploader
+import logging
 # Create your views here.
 
+logger = logging.getLogger(__name__)
 
 class RecruiterProfileView(RetrieveUpdateAPIView):
     permission_classes = [IsRecruiter]
@@ -15,4 +13,14 @@ class RecruiterProfileView(RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
-    
+
+    def perform_update(self, serializer):
+
+        image = self.request.FILES.get("profile_picture")
+        profile_data = {}
+
+        if image:   
+            image_url = cloudinary.uploader.upload(image)["secure_url"]
+            logger.info(f"Uploaded image URL: {image_url}")
+            profile_data["profile_picture"] = image_url
+        serializer.save(recruiterprofile=profile_data if profile_data else None)
